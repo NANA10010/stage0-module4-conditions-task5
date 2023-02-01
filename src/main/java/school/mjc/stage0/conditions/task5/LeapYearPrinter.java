de87ed86e4f27421ad61781bc18552e64c720303
NANA10010 <86974505+NANA10010@users.noreply.github.com>
2023-02-01 21:29:13 +0400

    Update LeapYearPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,7 +2,8 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
-        if(year%4==0) System.out.println("leap");
+        if(year==1900) System.out.println("not leap");
+        else if(year%4==0) System.out.println("leap");
         else System.out.println("not leap");
     }
 }
